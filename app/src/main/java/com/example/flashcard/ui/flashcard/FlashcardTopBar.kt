2336e04d0f2c99dd39c89d9fe65e9d8a5535e4eb
ajay <ajayjarwal44@gmail.com>
2024-06-25 18:22:49 +0530

    changed background of flashcard top app bar

--- app/src/main/java/com/example/flashcard/ui/flashcard/FlashcardTopBar.kt
+++ app/src/main/java/com/example/flashcard/ui/flashcard/FlashcardTopBar.kt
@@ -1,18 +1,28 @@
 package com.example.flashcard.ui.flashcard
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.flashcard.R
 
 
@@ -28,23 +38,33 @@ fun FlashcardTopBar(
     TopAppBar(
         modifier = modifier,
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-            titleContentColor = MaterialTheme.colorScheme.onPrimary
+            containerColor = Color.Transparent,
+            titleContentColor = Color.Blue
         ),
         title = {
             Text(
                 text = title,
+                fontSize = 24.sp
             )
         },
         navigationIcon = {
             if (canClose) {
                 IconButton(onClick = onClose) {
                     Icon(
+                        modifier = Modifier.size(44.dp),
                         imageVector = Icons.Filled.Close,
                         contentDescription = "close"
                     )
                 }
             }
         }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FlashcardTopBarPreview() {
+    FlashcardTopBar(
+        canClose = true
     )
 }
